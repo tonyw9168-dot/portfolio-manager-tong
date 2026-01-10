becaa4ed46da66bd5546235f480e3685a950c996
tonyw9168-dot <tonyw9168@gmail.com>
2026-01-10 08:14:49 +0000

    Add database data backup

---
+++ db_backup/data.sql
@@ -0,0 +1,120 @@
+mysqldump: [Warning] Using a password on the command line interface can be insecure.
+-- MySQL dump 10.13  Distrib 8.0.43, for Linux (x86_64)
+--
+-- Host: gateway01.eu-central-1.prod.aws.tidbcloud.com    Database: test
+-- ------------------------------------------------------
+-- Server version	8.0.11-TiDB-v7.5.2-serverless
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `asset_categories`
+--
+
+LOCK TABLES `asset_categories` WRITE;
+/*!40000 ALTER TABLE `asset_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `asset_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `asset_values`
+--
+
+LOCK TABLES `asset_values` WRITE;
+/*!40000 ALTER TABLE `asset_values` DISABLE KEYS */;
+/*!40000 ALTER TABLE `asset_values` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `assets`
+--
+
+LOCK TABLES `assets` WRITE;
+/*!40000 ALTER TABLE `assets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `assets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `cash_flows`
+--
+
+LOCK TABLES `cash_flows` WRITE;
+/*!40000 ALTER TABLE `cash_flows` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cash_flows` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `exchange_rates`
+--
+
+LOCK TABLES `exchange_rates` WRITE;
+/*!40000 ALTER TABLE `exchange_rates` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exchange_rates` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `family_members`
+--
+
+LOCK TABLES `family_members` WRITE;
+/*!40000 ALTER TABLE `family_members` DISABLE KEYS */;
+INSERT INTO `family_members` VALUES (1,'张三','户主','本人',NULL,35,0,'2026-01-10 07:13:36','2026-01-10 07:13:36');
+/*!40000 ALTER TABLE `family_members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `insurance_policies`
+--
+
+LOCK TABLES `insurance_policies` WRITE;
+/*!40000 ALTER TABLE `insurance_policies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `insurance_policies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `portfolio_summary`
+--
+
+LOCK TABLES `portfolio_summary` WRITE;
+/*!40000 ALTER TABLE `portfolio_summary` DISABLE KEYS */;
+/*!40000 ALTER TABLE `portfolio_summary` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `snapshots`
+--
+
+LOCK TABLES `snapshots` WRITE;
+/*!40000 ALTER TABLE `snapshots` DISABLE KEYS */;
+/*!40000 ALTER TABLE `snapshots` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'local-user','Local User',NULL,'local','user','2026-01-10 07:12:02','2026-01-10 07:17:32','2026-01-10 07:17:32');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-01-10  3:13:14
